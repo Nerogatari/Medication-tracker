1dbe76a3f1b7f7635fe71bae0d63d19515112d6b
Yan Yu <shintaro@drrr.us>
2024-04-11 19:45:48 -0600

    adding DB connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected successfully.'))
+.catch(err => console.log(err));
 
 app.use(express.json());  // Middleware to parse JSON bodies
 
